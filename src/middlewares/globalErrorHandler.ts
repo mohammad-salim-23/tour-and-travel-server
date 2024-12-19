3094cff47f0c75158fab8aea5ba43b2d2e5d26d4
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-12-19 22:54:28 +0600

    globalerror handler logic update

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction } from "express"
+import { NextFunction, Response } from "express"
 import { StatusCodes } from "http-status-codes"
+import { handleZodError } from "../helpers/handleZodError"
+import mongoose from "mongoose";
+import { handleValidationError } from "../helpers/handleValidationError";
+import { handleDuplicateError } from "../helpers/handleDuplicateError";
+import { handleGenericError } from "../helpers/handleGenericError";
 
 //Error:
 //Generic Error->js teke ase
@@ -11,7 +16,15 @@ import { StatusCodes } from "http-status-codes"
 //4.Zod Error/joi
 
 export const globalErrorHandler=((err:any,req:Request, res:Response,_next:NextFunction)=>{
-  console.log('error from app.ts',err)
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR)
-  .json({success:false,message:err.message,error:err})
+  if(err.name && err.name==="ZodError"){
+    handleZodError(err,res);
+  }
+  else if(err instanceof mongoose.Error.ValidationError){
+    handleValidationError(err,res);
+  }else if(err.code && err.code === 11000){
+    handleDuplicateError(err,res);
+  }
+  else if(err instanceof Error){
+    handleGenericError(err,res);
+  }
 })
