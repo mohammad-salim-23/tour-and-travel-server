88e1f8bfc9103680f59f6d86c4d2c33a6ce999d5
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-12-19 21:56:25 +0600

    helpers error create for globalErrorHandler

---
+++ src/helpers/handleValidationError.ts
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { Response } from "express";
+
+export const handleValidationError = (err:any, res:Response)=>{
+    const issues = Object.values(err.errors).map((item:any)=>{
+        return{
+            name:item.name,
+            path:item.path,
+            message:item.message
+        }
+    });
+    res.status(400).json({
+        success: false,
+        message: err.message,
+        issues: issues,
+        error: err
+    })
+}
